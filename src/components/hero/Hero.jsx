922270607461278b83fa781f2d130b2225ecab8d
Otabek <abdullayevriza80@gmail.com>
2026-04-23 20:13:57 +0500

    wert

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,11 +4,11 @@ import Rectangle2 from "../../assets/Rectangle (1).png"
 function Hero() {
   return (
     <div className="items-center text-center container">
-      <h1 className="text-[#212B31] text-[48px] font-[400] w-[818px]  flex justify-center m-auto  mt-[70px]">Умная доставка материалов для сложных строительных площадок</h1>
+      <h1 className="text-[#212B31] text-[48px] font-[400] w-[818px]  flex justify-center m-auto  inter mt-[70px]">Умная доставка материалов для сложных строительных площадок</h1>
 
       <div className="flex items-center justify-center gap-[16px] mt-[30px] mb-[80px]">
-        <button className="text-[#FFFFFF] text-[17px] font-[400] px-[25px] py-[13px] bg-[#212B31] rounded-[4px]">Заказать демо</button>
-        <button className="text-[#000000] text-[17px] font-[400] px-[25px] py-[13px] border-[#DADEDF] border-[2px] rounded-[4px]">Подробнее</button>
+        <button className="text-[#FFFFFF] text-[17px] font-[400] px-[25px] py-[13px] bg-[#212B31] inter rounded-[4px]">Заказать демо</button>
+        <button className="text-[#000000] text-[17px] font-[400] px-[25px] py-[13px] border-[#DADEDF] inter border-[2px] rounded-[4px]">Подробнее</button>
       </div>
 
       <img src={Rectangle} alt="Rectangle"  className="mb-[35px] block m-auto"/>
--- src/components/hero2/Hero2.jsx
+++ src/components/hero2/Hero2.jsx
@@ -1,8 +1,17 @@
+import frame from "../../assets/Frame.png"
+import frame2 from "../../assets/Frame (1).png"
+import frame3 from "../../assets/Frame (2).png"
 
 function Hero2() {
   return (
-    <div>
-      otabek
+    <div className="flex mt-[140px] justify-between container mb-[50px]">
+      <div className="text-center">
+        <img src={frame} alt="frame" />
+        <h4 className="text-[#212B31] text-[18px] font-[400] mt-[20px] mb-[10px] ">Повышение эффективности</h4>
+        <p className="text-[#72787D] w-[325px] text-[16px] font-[400]  ">Эффективные проекты стоят дешевле и завершаются в срок.</p>
+      </div>
+
+      {/* 1 */}
     </div>
   )
 }
