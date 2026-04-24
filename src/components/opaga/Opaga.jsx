206e726bf9b95adb15d3b7a30d262adf00217ad2
Otabek <abdullayevriza80@gmail.com>
2026-04-24 16:16:13 +0500

    wert

--- src/components/opaga/Opaga.jsx
+++ src/components/opaga/Opaga.jsx
@@ -1,14 +1,8 @@
 
  function Opaga() {
   return (
-    <div className="flex container bg-[#FFD042]  h-[466px] items-center px-[38px] justify-center text-center mb-[70px]">
-      <div className="">
-        <h3 className="text-[#212B31] text-[48px] w-[980px] mb-[54px] font-[400] m-auto">Готовы оптимизировать логистику доставки для вашего сложного проекта?</h3>
-      <div className="flex items-center gap-[16px] justify-center">
-        <button className="bg-[#212B31] rounded-[4px] px-[25px] py-[13px] text-[#fff] text-[18px] font-[400]">Заказать демо</button>
-        <button className="bg-[#fff] rounded-[4px] px-[25px] py-[13px] text-[#212B31] text-[18px] font-[400]">Связаться с нами</button>
-      </div>
-      </div>
+    <div className="">
+      
     </div>
   )
 }
